8f51db675c5c1d4c85ee3bfdc02aec811b5fc6d6
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-01 22:40:25 -0500

    feat(configuration.nix): removed unnecesary comments + config

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   grub-pkgs,
   config,
@@ -34,11 +31,6 @@
   };
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Set your time zone.
   time.timeZone = "America/Lima";
@@ -47,25 +39,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver = {
-  #   layout = "latam";
-  #   xkbVariant = "";
-  # };
-
-  # Configure console keymap
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "la-latin1";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -79,21 +52,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.guest = {
+  users.users."guest" = {
     isNormalUser = true;
-    description = "Luciano";
+    description = "Guest user";
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       firefox
@@ -103,32 +68,6 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
