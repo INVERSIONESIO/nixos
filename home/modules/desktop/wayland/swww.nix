b2294767be20bdb751792f75ea513c419b1b5759
Raiden <luis.quinones@inversiones.io>
2024-03-19 14:03:08 -0500

    feat(desktop): new default wallpaper

--- home/modules/desktop/wayland/swww.nix
+++ home/modules/desktop/wayland/swww.nix
@@ -4,7 +4,7 @@
   xdg.configFile = {
     "hypr/hyprland.conf".text = ''
       exec-once = ${pkgs.swww}/bin/swww init
-      exec = ${pkgs.swww}/bin/swww img ${../dots/background.gif}
+      exec = ${pkgs.swww}/bin/swww img ${../dots/background.png}
     '';
   };
 }
