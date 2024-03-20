1c192d9ba7d2d331a20608ec6e999da288150f7d
Raiden <luis.quinones@inversiones.io>
2024-03-20 11:33:21 -0500

    feat(macos-ventura): new vm settings

--- system/modules/vm/macos-ventura.nix
+++ system/modules/vm/macos-ventura.nix
@@ -4,7 +4,12 @@
     package = nixtheplanet.legacyPackages.x86_64-linux.makeDarwinImage {
       diskSizeBytes = 120000000000; # 120GB
     };
+    cores = 4;
+    threads = 8;
+    mem = "12G";
     openFirewall = true;
     vncListenAddr = "0.0.0.0";
+    autoStart = false;
+    stateless = false;
   };
 }
