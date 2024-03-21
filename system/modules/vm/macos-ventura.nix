52e327d0b38dd090509852ad1ea9bd7d9e758299
Raiden <luis.quinones@inversiones.io>
2024-03-21 10:14:16 -0500

    feat(macos-ventura): assigned more resources for vm

--- system/modules/vm/macos-ventura.nix
+++ system/modules/vm/macos-ventura.nix
@@ -4,9 +4,9 @@
     package = nixtheplanet.legacyPackages.x86_64-linux.makeDarwinImage {
       diskSizeBytes = 120000000000; # 120GB
     };
-    cores = 4;
-    threads = 8;
-    mem = "12G";
+    cores = 8;
+    threads = 16;
+    mem = "16G";
     openFirewall = true;
     vncListenAddr = "0.0.0.0";
     autoStart = false;
